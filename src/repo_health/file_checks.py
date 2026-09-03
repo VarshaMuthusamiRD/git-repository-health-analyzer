e1bba73ea256d72a37cb8609598ae2bb2938fb7c
Varsha Muthusami <varsha.muthusami@rapiddata.com>
2026-09-03 10:59:32 +0530

    Add repository analyzer interface stubs

---
+++ src/repo_health/file_checks.py
@@ -0,0 +1,10 @@
+def find_todo_fixme(repo_path):
+    pass
+
+
+def check_tests(repo_path):
+    pass
+
+
+def check_documentation(repo_path):
+    pass
---
+++ src/repo_health/git_checks.py
@@ -0,0 +1,10 @@
+def check_git_status(repo_path):
+    pass
+
+
+def get_recent_commits(repo_path):
+    pass
+
+
+def get_branches(repo_path):
+    pass
